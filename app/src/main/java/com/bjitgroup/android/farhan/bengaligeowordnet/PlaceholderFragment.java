5bac501e7de267daa77f2dd6c69e9bfc54cbeff5
Farhan Ar Rafi <farhan.rafi@bjitgroup.com>
2015-10-20 17:34:06 +0600

    Adding Login Functionality - Received Session

--- app/src/main/java/com/bjitgroup/android/farhan/bengaligeowordnet/PlaceholderFragment.java
+++ app/src/main/java/com/bjitgroup/android/farhan/bengaligeowordnet/PlaceholderFragment.java
@@ -23,12 +23,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -138,8 +143,9 @@ public class PlaceholderFragment extends Fragment implements View.OnClickListene
             }
             String userName = bUsername.getText().toString();
             String passWord = bPassword.getText().toString();
+
             //errorText.setVisibility(View.INVISIBLE);
-            Toast.makeText(getActivity(), "LOGIN BUTTON", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getActivity(), "LOGIN BUTTON", Toast.LENGTH_SHORT).show();
             if (validateLogin(userName, passWord).equals("")) {
                 errorText.setVisibility(View.VISIBLE);
             }
@@ -188,20 +194,36 @@ public class PlaceholderFragment extends Fragment implements View.OnClickListene
     private String validateLogin(String username, String password) {
         RequestQueue queue = Volley.newRequestQueue(getActivity());
         String url = "http://www.farhanarrafi.com/user/login.html";
-
+        final String uname = username;
+        final String pass = password;
         StringRequest request = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Log.i(TAG, response.toString());
-                        Toast.makeText(getActivity(), "Response is: " + response.substring(0, 500), Toast.LENGTH_LONG).show();
+                        //Toast.makeText(getActivity(), "Response is: " + response.substring(0, 500), Toast.LENGTH_LONG).show();
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getActivity(), "Volley didn't work", Toast.LENGTH_SHORT).show();
             }
-        });
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", uname);
+                params.put("password", pass);
+                return params;
+
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                Log.i(TAG, response.headers.get("Set-Cookie"));
+                return super.parseNetworkResponse(response);
+            }
+        };
         queue.add(request);
 
         //
